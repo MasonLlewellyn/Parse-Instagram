0ec8527bd84c01764bffd18df19c0f45d4a5b7bd
Mason Llewellyn <masonllewellyn@fb.com>
2020-07-06 19:30:19 -0400

    Initial Commit

---
+++ Parse-Instagram/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Parse-Instagram
+//
+//  Created by Mason Llewellyn on 7/6/20.
+//  Copyright © 2020 Facebook University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Parse-Instagram/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Parse-Instagram
+//
+//  Created by Mason Llewellyn on 7/6/20.
+//  Copyright © 2020 Facebook University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
